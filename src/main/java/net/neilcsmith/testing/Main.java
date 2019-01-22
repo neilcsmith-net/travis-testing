310d3ad3cfffbe2b29303ef67c7b21bb505d9cc7
Neil C Smith <neilcsmith.net@googlemail.com>
2019-01-22 18:08:02 +0000

    Initial commit

---
+++ src/main/java/net/neilcsmith/testing/Main.java
@@ -0,0 +1,13 @@
+package net.neilcsmith.testing;
+
+/**
+ *
+ * @author Neil C Smith - https://www.neilcsmith.net
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("We go testing!");
+    }
+    
+}
